d834b6d142ec66715b0ad919cbc381196ec522ce
Diawanti Danti <diawantiu@gmail.com>
2021-03-02 22:04:45 +0700

    first commit

---
+++ resources/views/register.blade.php
@@ -0,0 +1,36 @@
+<!DOCTYPE html>
+<html>
+    <head>
+        <title>Register</title>
+    </head>
+    <body>
+        <div class="container">
+            <h1>Buat Account Baru!</h1>
+            <h2>Sign Up Form</h2>
+            
+                <label>First name:</label> <br>
+                <input type="text"><br>
+                <label>Last name:</label> <br>
+                <input type="text"><br>
+                <label>Gender:</label> <br>
+                <input type="radio" name="gender" value="male">Male <br>
+                <input type="radio" name="gender" value="female">Female <br>
+                <input type="radio" name="gender" value="other">Other <br>
+                <label>Nationality:</label> <br>
+                <select name='nationality'>
+                    <option value='Indonesia'>Indonesia</option>
+                    <option value='singapura'>Singapura</option>
+                    <option value='malaysia'>Malaysia</option>
+                    <option value='australia'>Australia</option>
+                </select> <br>
+                <label>Language Spoken:</label> <br>
+                <input type="checkbox"> Bahasa Indonesia <br>
+                <input type="checkbox"> English <br>
+                <input type="checkbox"> Other <br>
+                <label>Bio:</label> <br>
+                <textarea></textarea> <br>
+                <a href='welcomee'><button type="submit">Sign Up</button>
+            </form>
+        </div>
+    </body>
+</html>
